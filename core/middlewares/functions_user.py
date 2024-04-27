e4f24828f9eee735f42aca2712938685492ea3a5
onetaper-gleb <tigertankp@gmail.com>
2024-04-27 10:16:06 +0300

    BANAANNANANANNANANANANANNANANANANn

--- core/middlewares/functions_user.py
+++ core/middlewares/functions_user.py
@@ -1,14 +1,21 @@
 import sqlite3
 
 
-def get_codes():
+def get_codes(code):
+    result = S_F('code', 'Surveys')
+    for i in result:
+        if code in i:
+            return True
+    return False
+
+
+def S_F(row, table):
     con = sqlite3.connect("../../Questionnaire.db")
     cur = con.cursor()
-    result = cur.execute(f"""SELECT code FROM Surveys""").fetchall()
-    print(result)
+    if type(row) == str:
+        result = cur.execute(f"""SELECT {row} FROM {table}""").fetchall()
+    else:
+        result = cur.execute(f"""SELECT {', '.join(row)} FROM {table}""").fetchall()
     con.commit()
     con.close()
     return result
-
-
-print(get_codes())
